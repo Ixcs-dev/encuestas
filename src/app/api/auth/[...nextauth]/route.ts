f093affb1678a2e2e195f00cecf9d6e4d24eec26
Yhonayker <ixcsgm@gmail.com>
2024-10-04 16:24:23 -0500

    update

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,31 @@
+import NextAuth, { AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+// Instancia de PrismaClient
+const prisma = new PrismaClient();
+
+// Opciones de autenticación
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  pages: {
+    signIn: "/",
+  },
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      return url.startsWith(baseUrl) ? url : baseUrl + "/";
+    },
+  },
+};
+
+// Manejo de las rutas GET y POST
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
