da58253001c8403af8f1ba3fcacabb2565f16835
Yhonayker <ixcsgm@gmail.com>
2024-10-04 15:24:58 -0500

    Update index.ts

--- src/app/api/encuesta/index.ts
+++ src/app/api/encuesta/index.ts
@@ -1,18 +1,24 @@
-// pages/api/encuestas/index.ts
-import { PrismaClient } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'GET') {
+  if (req.method === 'POST') {
     try {
-      const encuestas = await prisma.encuesta.findMany();
-      res.status(200).json(encuestas);
+      const data = req.body;
+      const encuesta = await prisma.encuesta.create({
+        data: {
+          ...data,
+        },
+      });
+      res.status(200).json(encuesta);
     } catch (error) {
-      res.status(500).json({ error: 'Error obteniendo encuestas' });
+      console.error(error);
+      res.status(500).json({ error: 'Error al guardar la encuesta' });
     }
   } else {
-    res.status(405).json({ error: 'Método no permitido' });
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Método ${req.method} no permitido`);
   }
 }
